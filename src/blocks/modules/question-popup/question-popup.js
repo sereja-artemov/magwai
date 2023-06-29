088f4315e647d67dc18160e13a4eb592d3bf51f6
sereja-artemov <artemov46@gmail.com>
2023-06-29 16:18:03 +0300

    features: реализовал валидацию формы

--- src/blocks/modules/question-popup/question-popup.js
+++ src/blocks/modules/question-popup/question-popup.js
@@ -1,7 +1,10 @@
+import validator from 'validator';
+
 let closeBtn = document.querySelector('.modal-content__close');
 let overlay = document.querySelector('.overlay');
-let form = document.querySelector('.modal-content');
+let modalPopup = document.querySelector('.modal-content');
 let ctaBtns = document.querySelectorAll('[data-type="question"]');
+let form = document.querySelector('.question-form');
 
 let isModalOpen = false;
 
@@ -35,12 +38,72 @@ document.addEventListener('keydown', function (e) {
 
 function closePopup() {
   overlay.style.display = 'none';
-  form.style.display = 'none';
+  modalPopup.style.display = 'none';
   isModalOpen = false;
 }
 
 function openPopup() {
   overlay.style.display = 'block';
-  form.style.display = 'block';
+  modalPopup.style.display = 'block';
   isModalOpen = true;
+}
+
+// валидация формы
+let nameField = modalPopup.querySelector('.question-form__name');
+let emailField = modalPopup.querySelector('.question-form__email');
+let telField = modalPopup.querySelector('.question-form__tel');
+let formFields = modalPopup.querySelectorAll('.question-form__field');
+let messsage = modalPopup.querySelector('.modal-content__message');
+let checkboxField = modalPopup.querySelector('.question-form__moms-checkbox-item');
+let checkboxLabel = modalPopup.querySelector('.question-form__moms-checkbox');
+
+form.onsubmit = function () {
+
+  if (!validator.isEmpty(nameField.value)) {
+    nameField.classList.remove('field-error');
+    messsage.textContent = '';
+  } else {
+    nameField.classList.add('field-error');
+    messsage.textContent = 'Введите имя';
+    return false;
+  }
+
+  if (validator.isEmail(emailField.value)) {
+    emailField.classList.remove('field-error');
+    messsage.textContent = '';
+  } else {
+    emailField.classList.add('field-error');
+    messsage.textContent = 'Некорректно введен Email';
+    return false;
+  }
+
+  if (validator.isMobilePhone(telField.value, 'ru-RU')) {
+    telField.classList.remove('field-error');
+    messsage.textContent = '';
+  } else {
+    telField.classList.add('field-error');
+    messsage.textContent = 'Некорректный номер телефона';
+    return false;
+  }
+
+  if (!checkboxField.checked) {
+    messsage.textContent = 'Вы школьник? Поставьте галочку, если нет.';
+    checkboxLabel.classList.add('errorColor');
+    return false;
+  } else {
+    messsage.textContent = '';
+    checkboxLabel.classList.remove('errorColor');
+  }
+
+  Array.from(formFields).forEach((field) => {
+    if (validator.isEmpty(field.value)) {
+      field.classList.add('field-error');
+      messsage.textContent = 'Заполните все поля';
+      return false;
+    } else {
+      field.classList.remove('field-error');
+      messsage.textContent = '';
+    }
+  });
+
 }
